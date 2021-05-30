4ff8a581559eec28fc8e19c54bf10b0b5a069c27
Avivit Weissman <wavivit@gmail.com>
2021-05-30 05:36:57 +0000

    added helper functions for db

--- server/database.js
+++ server/database.js
@@ -1,21 +1,57 @@
 const db = require('./dbconnect');
 
-// Example from lightBNB
-const getUserWithEmail = function(email) {
+// Get all the lists for a user
+const getAllMyLists = function(id) {
   const query = db.query(
-    `SELECT *
-    FROM users
-    WHERE email LIKE $1;`, [email])
+    `SELECT name, description,priority, is_checked
+    FROM lists
+    JOIN users On lists.user_id = users.id
+    Where users.id = $1;
+    GROUP BY list_type
+    LIMIT 4
+    ORDER BY lists.id DESC
+    ;`, [id])
   .then(res=>{
-    return res.rows[0];
+    return res.rows;
   })
   .catch(err=>{console.log(err)});
   return query;
 };
 
-exports.getUserWithEmail = getUserWithEmail;
+//Get a specific list for a user
+const getList = function(id, list_type) {
+  const query = db.query(
+    `SELECT name, description,priority, is_checked
+    FROM lists
+    JOIN users On lists.user_id = users.id
+    Where users.id = $1
+    AND list_type = $2
+    GROUP BY list_type
+    LIMIT 4
+    ORDER BY lists.id DESC;`, [id, list_type])
+  .then(res=>{
+    return res.rows;
+  })
+  .catch(err=>{console.log(err)});
+  return query;
+};
 
-// functions that we are going to need
+//Insert an item to a list
+const insertToDoITEm = function(property) {
+  const query = db.query(
+    ` INSERT INTO lists(name, description)
+    VALUES($1, $2)
+    RETURNING *;`, [property])
+  .then(res=>{
+    return res.rows;
+  })
+  .catch(err=>{console.log(err)});
+  return query;
+};
 
-// getUserItems = query the DB for all rows for a user
+exports.getAllMyLists = getAllMyLists;
+exports.getList = getList;
+exports.insertToDoITEm = insertToDoITEm;
+
+
 
