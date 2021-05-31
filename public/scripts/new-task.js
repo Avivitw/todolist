9b4ab4f2db52ecf0c20a5f412d1468b88a0100b9
archaemedes <wadecroft@gmail.com>
2021-05-31 01:25:08 +0000

    Wrote handler logic for when the add task button is pressed, also found an interesting API for categorizing text

--- public/scripts/new-task.js
+++ public/scripts/new-task.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
   const addButton = function() {
     const text = $('.new-task-text').val();
     if (text.length > 0 && !appended) {
-      $('.new-todo').append(`<button class='add-task-button'for='new-todo-form'type="submit">Add</button>`);
+      $('.new-todo').append(`<button class='add-task-button'for='new-todo-form'type="submit"><i class="fas fa-plus"></i></button>`);
+      $('.add-task-button').click(()=>{
+        // Listener for the add task button
+      })
       appended = true;
     }
     if (text.length === 0) {
@@ -13,6 +16,21 @@ $(document).ready(function(){
     }
   }
 
+
+
+  // Listeners to show the add task button
   $('.new-task-text').keydown(addButton);
   $('.new-task-text').keyup(addButton);
+
+  $('.add-task-button').click(()=>{
+    console.log('add clicked');
+    // $.ajax({
+    //   url:"https://api.monkeylearn.com/v3/classifiers/cl_o46qggZq/classify/",
+    //   method:"POST",
+    //   data: ["Grapefruit"],
+    //   beforeSend: function (xhr) {
+    //     xhr.setRequestHeader("Authorization", "Token " + "11284b94944fd21cd3e16ad2771c02106ec38995");
+    //   }
+    // });
+  })
 });
