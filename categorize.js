3392d3e854788042f5c7f421d90f42f4c1fdf77e
archaemedes <wadecroft@gmail.com>
2021-06-01 20:54:09 +0000

    removed unused functions from file and refactored the main search function

--- categorize.js
+++ categorize.js
@@ -1,175 +1,63 @@
 require("dotenv").config();
+const e = require("express");
 const { response } = require("express");
 const request = require("request");
+const keywords = require('./keywords');
 const args = process.argv[2];
 
-const generalSearch = function (query) {
-  // Unique appId from Wolfram
-  const appId = process.env.WOLFRAM_ID;
-  // Encode query for URL insertion
-  const encodedQuery = encodeURIComponent(query);
-  // Wolfram Api URL
-  const url = `http://api.wolframalpha.com/v2/query?appid=${appId}&input=${encodedQuery}&output=json`;
-  // Avg query time for 'full-data' Wolfram endpoint is about 7.5 seconds
-  request(url, function (error, response, body) {
-    // Data 'pods' retrieved from the server
-    const data = JSON.parse(body);
-    const assumedTypes = data.queryresult.datatypes;
-    // true or false if search in their db was successful
-    console.log("assumed type:", assumedTypes);
-    console.log("error:", error);
-    console.log("statusCode:", response && response.statusCode);
-    console.log("body:", data);
-  });
-};
-
-const searchMovie = function (query) {
-  // Example request https://api.themoviedb.org/3/movie/550?api_key=8eb5f5dee8a6b4179174ab1bb9af5f57
-  // API Key
-  const appKey = process.env.MOVIEDB_ID;
-  // Encode query for URL insertion
-  const encodedQuery = encodeURIComponent(query);
-  // Movie search URL
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${appKey}&query=${encodedQuery}`;
-  request(url, function (error, response, body) {
-    const data = JSON.parse(body);
-    const firstEntry = data.results[0];
-    const popularity = firstEntry.popularity;
-    const receivedTitle = firstEntry.title;
-    console.log("error:", error);
-    console.log("statusCode:", response && response.statusCode);
-    console.log("body:", firstEntry);
-    console.log("popularity:", popularity);
-  });
-};
-
-const bookSearch = function (query) {
-  // example search GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
-  const appKey = process.env.BOOKS_ID;
-  // Encode query for URL insertion
-  const encodedQuery = encodeURIComponent(query);
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${encodedQuery}&key=${appKey}`;
-  request(url, function (error, response, body) {
-    const data = JSON.parse(body);
-    const totalItemsFound = data.totalItems;
-    console.log("error:", error);
-    console.log("statusCode:", response && response.statusCode);
-    console.log("body:", data);
-    console.log("total items", data.totalItems);
-  });
-};
-
-// This function uses the SERP API to search google
-const serpSearch = function (query) {
-  const encodedQuery = query.replace(/\s/g, "+");
-  console.log(encodedQuery);
-  const appKey = process.env.SERP_ID;
-  const url = `https://serpapi.com/search.json?engine=google&q=${encodedQuery}&location=Canada&google_domain=google.ca&gl=ca&hl=en&api_key=${appKey}`;
-  request(url, function (error, response, body) {
-    const data = JSON.parse(body);
-    const receivedType = data.knowledge_graph.type;
-    console.log("error:", error);
-    console.log("statusCode:", response && response.statusCode);
-    console.log("body:", body);
-    console.log("google type:", receivedType);
-    // Check if word novel is in google description
-    if (receivedType.toLowerCase().includes("novel")) {
-      console.log("added to book list");
-    }
-    if (receivedType.toLowerCase().includes("fruit")) {
-      console.log("added to eat list");
-    }
-  });
-};
-
+// This function returns a guess 'e', 'w', 'r', 'b' to categorize item
 const googleSearch = function (query) {
   const appKey = process.env.GOOGLE_SEARCH_API_KEY;
   const encodedQuery = query.replace(/\s/g, "+");
   const url = `https://kgsearch.googleapis.com/v1/entities:search?query=${encodedQuery}&key=${appKey}&limit=1&indent=True`;
+  // Promise to retrieve category guess from google
   return new Promise(function(resolve, reject) {
     request(url, function (error, response, body) {
         const data = JSON.parse(body);
         console.log("error:", error);
-        console.log("statusCode:", response && response.statusCode);
         // Get the types array returned from google via a workaround for the @ symbol
         const typeWorkaround = "@type";
         const types = data.itemListElement[0].result[typeWorkaround];
         // description of item from google, sometimes undefined
-        const description = data.itemListElement[0].result.description;
-        console.log("description:", description);
+        const googleDescription = data.itemListElement[0].result.description;
+        console.log("description:", googleDescription);
         console.log("type:", types);
         // Checks description first for keywords
         // Eat list keywords
-        const eatKeywords = [
-          "fruit",
-          "pasta",
-          "food",
-          "dish",
-          "cake",
-          "dairy",
-          "restaurant"
-        ];
-
-        const watchKeywords = [
-          "movie"
-        ];
-
-        const readKeywords = [
-          "book"
-        ];
-
-        const buyKeywords = [
-          "product"
-        ];
-
         const googleTypes = types.join(" ").toLowerCase();
-
-        // Checks description for eat keywords
-        for (const keyword of eatKeywords) {
-          if (description && description.toLowerCase().includes(keyword)) {
-            console.log("added to eat list");
-            resolve('e');
-            return;
-          };
+        const keywordCheck = function(keywordObj){
+          const id = keywordObj.id;
+          const list = keywordObj.arrayOfKeywords;
+          for (const keyword of list) {
+            // Check avoids error if description is undefined
+            if (googleDescription) {
+              if (googleDescription.toLowerCase().includes(keyword)) {
+                console.log(`added to ${id} list`);
+                resolve(id);
+                return;
+              };
+            };
+            if (googleTypes) {
+              if (googleTypes.includes(keyword)) {
+                console.log(`added to ${id} list`);
+                resolve(id);
+                return;
+              };
+            };
+            };
         };
-        for (const keyword of watchKeywords) {
-          if (description && googleTypes.includes(keyword)) {
-            console.log("added to watch list");
-            resolve('w');
-            return;
-          };
-        };
-        for (const keyword of readKeywords) {
-          if (description && googleTypes.includes(keyword)) {
-            console.log("added to read list");
-            resolve('r')
-            return;
-          };
-        };
-        for (const keyword of buyKeywords) {
-          if (description && googleTypes.includes(keyword)) {
-            console.log("added to buy list");
-            resolve('b');
-            return;
-          };
-        };
-        // Second loop to categorize as thing if first loop fails to categorize
-        for (const type of types) {
-          if (type.toLowerCase().includes("thing")) {
-            console.log("added to shopping list");
-            resolve('b');
-            return;
-          };
-        };
+        // Checks description and type for eat keywords
+        keywordCheck(keywords.eat);
+        // Checks description and type for watch keywords
+        keywordCheck(keywords.watch);
+        // Checks description and type for read keywords
+        keywordCheck(keywords.read);
+        // Checks description and type for buy keywords
+        keywordCheck(keywords.buy);
       })
-
   })
 };
 
 module.exports = {
-  generalSearch,
-  searchMovie,
-  bookSearch,
-  googleSearch,
-  serpSearch
+  googleSearch
 };
