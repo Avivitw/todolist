63f26ec3a73b038f08f57b03e2bc1fee5b15821a
archaemedes <wadecroft@gmail.com>
2021-06-01 20:52:55 +0000

    added keywords for categorizing algorithm to a seperate file for organization

---
+++ keywords.js
@@ -0,0 +1,36 @@
+const keywords = {
+  eat: {
+    id: 'e',
+    arrayOfKeywords: [
+      "fruit",
+      "pasta",
+      "food",
+      "dish",
+      "cake",
+      "dairy",
+      "restaurant"
+    ]
+  },
+  watch: {
+    id: 'w',
+    arrayOfKeywords: [
+      "movie",
+      "tv show"
+    ]
+  },
+  read: {
+    id: 'r',
+    arrayOfKeywords: [
+      "book",
+      "novel"
+    ]
+  },
+  buy: {
+    id:'b',
+    arrayOfKeywords: [
+      "product"
+    ]
+  }
+}
+
+module.exports = keywords;
