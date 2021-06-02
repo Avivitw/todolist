0d5dae8dd7067048c1eb2e9f639d880c6ab7cfd6
Avivit Weissman <wavivit@gmail.com>
2021-06-02 17:19:52 +0000

    start to implemnt integration for adding new task to the db

--- public/scripts/new-task.js
+++ public/scripts/new-task.js
@@ -1,25 +1,16 @@
 $(document).ready(function(){
-  let appended = false;
 
   const addButton = function() {
     const text = $('.new-task-text').val();
-    if (text.length > 0 && !appended) {
-      $('.new-todo').append(`<button class='add-task-button'for='new-todo-form'type="submit"><i class="fas fa-plus"></i></button>`);
-      $('.add-task-button').click(()=>{
-        // Listener for the add task button
-      })
-      appended = true;
+    if (text.length > 0 ) {
+      $('.add-task-button').show();
     }
-    if (text.length === 0) {
-      $('.add-task-button').remove();
-      appended = false;
-    }
+    else {
+      $('.add-task-button').hide();
+          }
   }
 
-
-
   // Listeners to show the add task button
-  $('.new-task-text').keydown(addButton);
   $('.new-task-text').keyup(addButton);
 
   $('.add-task-button').click(()=>{
